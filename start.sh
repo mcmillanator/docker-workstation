bb36d901942b1d2e27872c5e824888110ce74d81
mcmillanator <stanalanmcm@gmail.com>
2020-11-05 10:12:24 -0500

    add --hostname to start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run  --rm -v ~/.ssh:/root/.ssh -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/root -it workstation /bin/bash
+docker run  --rm --hostname workstation -v ~/.ssh:/root/.ssh -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/root -it workstation /bin/bash
